e8b24c0d980678deb41acc6e270ecf7928c25a5a
AlexChumaslovZap <ac@zaptest.com>
2022-08-22 02:41:24 +0300

    Add Tailwind, basic layout and styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    content: ['./src/**/*.{html,ts}'],
+  },
+  // content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
